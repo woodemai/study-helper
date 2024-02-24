0cf92ae6f7a14120282b857dc49134ed608edf08
woodemai <n.savchenkoo73@gmail.com>
2024-02-25 01:48:36 +0300

    update packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,51 @@
 import path from "path"
 import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa";
+
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}"],
+      },
+      manifest: {
+        lang: "ru",
+        name: "Study Helper",
+        short_name: "Study Helper",
+        description:
+          "Твой помощник в учебе",
+        background_color: "#f2f2f2",
+        theme_color: "#f2f2f2",
+        display: "standalone",
+        icons: [
+          {
+            src: "icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "icons/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
